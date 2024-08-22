d8e486fc3c7c5147c22b356069611fddefbc6015
FELIPE PELIÇARI CANDIDO <2901392323013@fatecararas.com>
2024-08-22 19:59:20 -0300

    conceitos basicos de python na aula 2

---
+++ Aula_orlando/aula2.py
@@ -0,0 +1,22 @@
+# TIPOS NUMÉRICOS
+
+# numero1 = 10
+
+# print (numero1)
+# print (type(numero1))
+
+# numero2 = 10.4
+
+# print (type(numero2))
+
+
+
+
+lista = []
+lista.append('caguei mole')
+print(lista)
+lista.append('agora caguei duro')
+print(lista)
+
+for item in lista:
+    print(item)
